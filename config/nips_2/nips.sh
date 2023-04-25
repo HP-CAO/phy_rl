54a60d3f26aacea7ef5f6cd8a81d2ffe00fb1395
hongi <caohpone@gmail.com>
2023-04-25 21:30:19 +0200

    nips_train2

--- config/nips_2/nips.sh
+++ config/nips_2/nips.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env sh
 
 
-python main.py --config ./config/nips_2/nips_unk_our_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_no_res --mode train --force &
-python main.py --config ./config/nips_2/nips_unk_our_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_with_res --mode train --force &
-python main.py --config ./config/nips_2/nips_unk_ubc_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_no_res --mode train --force
-python main.py --config ./config/nips_2/nips_unk_ubc_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_with_res --mode train --force
+python main.py --config ./config/nips_2/nips_unk_our_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_no_res_1 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_our_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_with_res_1 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_no_res_1 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_with_res_1 --mode train --force &
+
+python main.py --config ./config/nips_2/nips_unk_our_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_no_res_2 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_our_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_with_res_2 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_no_res_2 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_with_res_2 --mode train --force &
+
+python main.py --config ./config/nips_2/nips_unk_our_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_no_res_3 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_our_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_our_with_res_3 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_no_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_no_res_3 --mode train --force &
+python main.py --config ./config/nips_2/nips_unk_ubc_with_res.json --params agent_params/experience_prefill_size 5000 --id nips_unk_ubc_with_res_3 --mode train --force
