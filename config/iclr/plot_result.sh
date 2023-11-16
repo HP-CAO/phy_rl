656b3ea49c15de9bab8b141a178c8e6733e88345
hongi <caohpone@gmail.com>
2023-11-16 11:48:43 +0100

    a

--- config/iclr/plot_result.sh
+++ config/iclr/plot_result.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
-python ./test/iclr/test_safety_envelope_mbrl_our.py &
-python ./test/iclr/test_safety_envelope_mbrl_ubc.py &
-python ./test/iclr/test_safety_envelope_mf_our.py &
-python ./test/iclr/test_safety_envelope_mf_ubc.py
+python ./test/test_safety_envelope_mbrl_our.py &
+python ./test/test_safety_envelope_mbrl_ubc.py &
+python ./test/test_safety_envelope_mf_our.py &
+python ./test/test_safety_envelope_mf_ubc.py
 
 
